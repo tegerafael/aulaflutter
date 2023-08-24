b7e212b837c1656b8aac46f61e069bc4a7a0ada2
Tiago Rafael <tiagorafael777@gmail.com>
2023-08-23 21:03:43 -0400

    feat: finish

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,42 @@ class Aula1 extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text("Título tela 1"),
+              title: Text("Informações Pessoais"),
               centerTitle: true,
             ),
             body: Column(
               children: [
                 Container(
+                  padding: EdgeInsets.all(10),
+                ),
+                Image(
+                  width: 300,
+                  height: 300,
+                  image: const AssetImage('assets/Tiago.png'),
+                ),
+                Icon(
+                  Icons.facebook,
+                  color: Colors.blue,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
                   child: Row(
                     children: [Text("Nome: "), Text("Tiago Rafael")],
                   ),
                 ),
                 Container(
+                  padding: EdgeInsets.only(left: 20),
                   child: Row(
                     children: [Text("Idade: "), Text("21")],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Row(
+                    children: [
+                      Text("Profissão: "),
+                      Text("Desenvolvedor Full Stack")
+                    ],
                   ),
                 ),
               ],
